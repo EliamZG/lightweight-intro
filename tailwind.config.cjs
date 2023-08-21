3566ae2c5f92674017c35af1295425a5469790fc
EliamZG <eliam.zamora@gmail.com>
2023-08-20 18:16:51 -0600

    WIP

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	mode: 'jit',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
+		screens: {
+			'sm' : '640px',
+			'md' : '992px',
+			'lg' : '1200px'
+		},
 		extend: {
+			fontFamily: {
+				'primary': ['Roboto']
+			},
 			colors: {
 				primary: '#402A17',
-				secondary: '#66532E',
-				accent: '#F2C777'
+				secondary: '#A67841',
+				accent: '#e8aa38',
+				base_dark: '#111'
 			}
 		},
 	},
